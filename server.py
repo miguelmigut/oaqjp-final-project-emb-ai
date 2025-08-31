5fc028ddcd1e464693894fd1a16e17711ca4090b
miguelmigut <miguelmiranda77@hotmail.com>
2025-08-31 05:42:35 -0400

    Incorporar manejo de errores

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
     return (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, "
